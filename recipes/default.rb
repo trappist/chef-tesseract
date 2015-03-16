cc4287e795b8abe0a073aa586070b1720fcf8501
Rocco Stanzione <grasshopper@linuxkungfu.org>
2015-03-16 15:15:10 -0500

    Set precedence

--- recipes/default.rb
+++ recipes/default.rb
@@ -65,7 +65,7 @@ execute "ldconfig" do
   command "/sbin/ldconfig"
 end
 
-node['tesseract'] ||= {}
+node.default['tesseract'] ||= {}
 node['tesseract']['dictionaries'] ||= ["eng"]
 
 for lang in node['tesseract']['dictionaries']
